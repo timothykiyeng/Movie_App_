371136c688603686ec970a2ce1d5553c3818b4b5
G-vans <jevans.otieno@student.moringaschool.com>
2022-11-10 06:51:57 +0300

    feat:add a review textarea

--- src/Components/AddReview.js
+++ src/Components/AddReview.js
@@ -1,35 +1,35 @@
-import { useState } from "react";
-import { FaStar } from "react-icons/fa";
+import React from "react";
+// import { FaStar } from "react-icons/fa";
 
-const colors = {
-    orange: "#FFBA5A",
-    grey: "#a9a9a9"
+// const colors = {
+//     orange: "#FFBA5A",
+//     grey: "#a9a9a9"
     
-};
+// };
 
 export default function AddReview() { 
 
-const [currentValue, setCurrentValue] = useState(0);
-const [hoverValue, setHoverValue] = useState(undefined);
-const stars = Array(5).fill(0)
+// const [currentValue, setCurrentValue] = useState(0);
+// const [hoverValue, setHoverValue] = useState(undefined);
+// const stars = Array(5).fill(0)
 
-const handleClick = value => {
-    setCurrentValue(value)
-  }
+// const handleClick = value => {
+//     setCurrentValue(value)
+//   }
 
-const handleMouseOver = newHoverValue => {
-    setHoverValue(newHoverValue)
-  };
+// const handleMouseOver = newHoverValue => {
+//     setHoverValue(newHoverValue)
+//   };
 
-const handleMouseLeave = () => {
-    setHoverValue(undefined)
-  }
+// const handleMouseLeave = () => {
+//     setHoverValue(undefined)
+//   }
    
 
     return (
         <div style={styles.container}>
-            <h2> Give Ratings and Reviews </h2>
-            <div style={styles.stars}>
+            <h2> Movie Review </h2>
+            {/* <div style={styles.stars}>
         {stars.map((_, index) => {
           return (
             <FaStar
@@ -46,14 +46,38 @@ const handleMouseLeave = () => {
             />
           )
         })}
-        </div>
+        </div> */}
+        <textarea
+        placeholder="Tell us more about the movie?"
+        style={styles.textarea}
+      />
+
+      <button
+        style={styles.button}
+      >
+        Submit
+      </button>
         </div>
     );
 };
 
 const styles = {
     container: {
-      display: "flex",
-      flexDirection: "column",
-      alignItems: "center"
-    }}
+        display: "flex",
+        flexDirection: "column",
+        alignItems: "center"
+      },
+    textarea: {
+        border: "1px solid #a9a9a9",
+        borderRadius: 5,
+        padding: 10,
+        margin: "20px 0",
+        minHeight: 100,
+        width: 300
+      },
+      button: {
+        border: "1px solid #a9a9a9",
+        borderRadius: 5,
+        width: 300,
+        padding: 10,
+      }}
