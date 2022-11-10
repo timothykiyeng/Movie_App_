402eeed1033adc7b9dd2dc1b2097eab9a164dfa3
Derrick Nyasio <dnyasio2014@gmail.com>
2022-11-10 15:38:21 +0300

    feat: Added css and react styling features

--- src/Components/Navbar.js
+++ src/Components/Navbar.js
@@ -1,24 +1,15 @@
 // import { Link } from 'react-router-dom';
+import React from "react";
+import { NavLink } from "react-router-dom";
 
 export default function Navbar() {
     return (
         <div className="Nav">
-            <div className='Logo'>
-                <ul>
-                    <li>
-                        <h1>Movie App</h1>
-                    </li>
-                </ul>
-            </div>
-            <div className='Nav-right'>
-                <ul>
-                    <li>All Movies</li>
-                    <li>All Reviews</li>
-                    <li>All Viewers</li>
-                    <li>
-                        <button className="Btn-navbar" ><a href='./'>Sign Up</a></button>
-                    </li>
-                </ul>
+            <h1>Movie App</h1>
+            <div className="links">
+                <NavLink to="/" exact className="topnav"> Movies </NavLink>
+                <NavLink to="/" exact className="topnav"> WatchList </NavLink>
+                <button className="Btn-navbar" ><a href='./'>Sign Up</a></button>
             </div>
         </div>
     );
