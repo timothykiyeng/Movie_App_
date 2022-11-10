b07acf80f95048d5731d54c10499776ccbbbe049
G-vans <jevans.otieno@student.moringaschool.com>
2022-11-10 06:41:37 +0300

    feat: setup default react-app file

---
+++ src/index.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import AddReview from './Components/AddReview';
+
+ReactDOM.render(
+  <React.StrictMode>
+    <AddReview />
+  </React.StrictMode>,
+  document.getElementById('root')
+);
