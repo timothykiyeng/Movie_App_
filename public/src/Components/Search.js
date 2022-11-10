7d38fb6ea03b95ee69dbbe70b854e0c1a79f5101
Moringa School <malvin.opondo@student.moringaschool.com>
2022-11-10 10:39:13 +0300

    feat: this commit adds input field

--- public/src/Components/Search.js
+++ public/src/Components/Search.js
@@ -2,7 +2,19 @@ import React from "react"
 
 function Search ({search, onSearchChange}) {
     return (
-        <div></div>
+        <div>
+
+            <header>
+
+                <input
+                className="search"
+                type= "text"
+                value={search}
+                placeholder="Search..."
+                onChange={onSearchChange}
+                />
+            </header>
+        </div>
     )
 }
 
