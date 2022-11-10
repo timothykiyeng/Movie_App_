edaa5129fc84764c6e8736110ac21ca696343d09
Timothy Kiyeng <timothy.kiyeng@student.moringaschool.com>
2022-11-10 10:22:04 +0300

    Imported useState hook and created my formData state and set my intialValues to it

--- src/Components/AddMovie.js
+++ src/Components/AddMovie.js
@@ -2,5 +2,5 @@ import React, {useState} from "react";
 
 const initialValues = { image_url: "", name: "", rating: "", overview: "" };
 function AddMovie ({ postMovies }){
-   
+    const [formData, setFormData] = useState(initialValues)
 }
